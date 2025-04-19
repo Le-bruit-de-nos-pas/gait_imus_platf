601624e2f0e91d82f5c86f5a263e8c0ba7721ef8
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2025-04-19 20:20:21 +0200

    Update dsp_book.py

--- dsp_book/dsp_book.py
+++ dsp_book/dsp_book.py
@@ -1,1 +1,12 @@
+def gregory_pi_approximation(n_terms):
+    pi_over_4 = 0
+    for n in range(n_terms+1):
+        term = ((-1)**n) / (2*n+1)
+        pi_over_4 += term
+    pi_approx = 4*pi_over_4
+    return pi_approx       
 
+
+approx_pi = gregory_pi_approximation(1000)
+
+print(f"Approximation = {approx_pi} ") 
